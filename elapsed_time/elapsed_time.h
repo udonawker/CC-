5bc03e4b78451097edccc4ba2882e461c0184d9a
udonawker <36081408+udonawker@users.noreply.github.com>
2020-10-11 04:43:39 +0900

    Update elapsed_time.h

--- elapsed_time/elapsed_time.h
+++ elapsed_time/elapsed_time.h
@@ -1,61 +1,25 @@
-#include <time.h>
-#include "elapsed_time.h"
+#include <cstdint>
 
-elapsed_time::elapsed_time(
-    int accuracy
-)
-    : start_(0.)
+class elapsed_time
 {
-    switch (accuracy) {
-    case ACCURACY_MSEC:
-        accuracy_ = 1000;
-        break;
-    case ACCURACY_NSEC:
-        accuracy_ = 1000 * 1000 * 1000;
-        break;
-    case ACCURACY_USEC:
-    default:
-        accuracy_ = 1000 * 1000;
-    }
-}
+public:
+    typedef enum _ACCURACY {
+        ACCURACY_MSEC,
+        ACCURACY_USEC,
+        ACCURACY_NSEC
+    } ACCURACY;
+private:
+    double          start_;
+    std::uint32_t   accuracy_;
 
-elapsed_time::elapsed_time(
-    const elapsed_time& elapsed_time
-)
-    : start_(elapsed_time.start_)
-    , accuracy_(elapsed_time.accuracy_)
-{
-}
+public:
+    elapsed_time(int accuracy = ACCURACY_USEC);
+    elapsed_time(const elapsed_time& elapsed_time);
+    ~elapsed_time();
 
-elapsed_time::~elapsed_time()
-{
-}
+    bool            start();
+    std::uint64_t   stop() const;
 
-bool elapsed_time::start()
-{
-    start_ = get_current();
-
-    return true;
-}
-
-std::uint64_t elapsed_time::stop() const
-{
-    double time = 0.;
-
-    if ((time = get_current()) < start_) {
-        return 0;
-    }
-
-    time -= start_;
-
-    return static_cast<std::uint64_t>(time * accuracy_);
-}
-
-inline double elapsed_time::get_current() const
-{
-    struct timespec ts{};
-
-    ::clock_gettime(CLOCK_MONOTONIC, &ts);
-
-    return ts.tv_sec + ((double)ts.tv_nsec * 1e-9);
-}
+private:
+    inline double   get_current() const;
+};
