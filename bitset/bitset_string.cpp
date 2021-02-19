68962094a5f1016d15fcf460ce331c50a63aaec7
udonawker <36081408+udonawker@users.noreply.github.com>
2021-02-19 15:40:33 +0900

    Update bitset_string.cpp

--- bitset/bitset_string.cpp
+++ bitset/bitset_string.cpp
@@ -8,15 +8,16 @@
 #include <iomanip>
 #include <bitset>
 
-void to_bit_string(std::string& bits)
+std::size_t to_bit_string(std::string& bits)
 {
     bits.erase(std::remove_if(bits.begin(), bits.end(), []( char ch ) { return !(ch == '0' || ch == '1'); }), bits.end());
+    return bits.length();
 }
 
 int main()
 {
     std::string bit_str("1111111111 1111111111 1111111111 1111111111");
-    to_bit_string(bit_str);
+    std::cout << "string.length() = " << to_bit_string(bit_str) << std::endl;
     std::cout << bit_str << std::endl;
     std::bitset<40> bits(bit_str);
     std::cout << "bits size = " << bits.size() << std::endl;
@@ -24,6 +25,7 @@ int main()
     return 0;
 }
 /*
+string.length() = 40
 1111111111111111111111111111111111111111
 bits size = 40
 */
